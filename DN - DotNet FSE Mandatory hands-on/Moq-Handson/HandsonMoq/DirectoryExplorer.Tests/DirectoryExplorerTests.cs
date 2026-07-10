379bc41d00aa67157ced9172714dee60dd2cdc28
Namratha Krishna Modugumudi <namrathakrishna74@gmail.com>
2026-07-10 12:09:59 +0530

    Add files via upload

---
+++ DN - DotNet FSE Mandatory hands-on/Moq-Handson/HandsonMoq/DirectoryExplorer.Tests/DirectoryExplorerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using MagicFilesLib;
+
+namespace DirectoryExplorer.Tests
+{
+    [TestFixture]
+    public class DirectoryExplorerTests
+    {
+        private readonly string _file1 = "file.txt";
+        private readonly string _file2 = "file2.txt";
+
+        private Mock<IDirectoryExplorer> mockDirectory;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            mockDirectory = new Mock<IDirectoryExplorer>();
+
+            mockDirectory
+                .Setup(x => x.GetFiles(It.IsAny<string>()))
+                .Returns(new List<string>
+                {
+                    _file1,
+                    _file2
+                });
+        }
+
+        [TestCase]
+        public void GetFiles_ReturnsTwoFiles()
+        {
+            var files = mockDirectory.Object.GetFiles("C:\\");
+
+            Assert.That(files, Is.Not.Null);
+            Assert.That(files.Count, Is.EqualTo(2));
+            Assert.That(files, Does.Contain(_file1));
+        }
+    }
+}
---
+++ DN - DotNet FSE Mandatory hands-on/NUnit-Handson/Hands-on/CalcLibraryTests/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibraryTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            calculator = null;
+        }
+
+        [TestCase(10, 20, 30)]
+        [TestCase(5, 5, 10)]
+        [TestCase(-5, 10, 5)]
+        [TestCase(0, 0, 0)]
+        public void TestAddition(int a, int b, int expected)
+        {
+            int actual = calculator.Add(a, b);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Ignore("Example Ignore Attribute")]
+        public void IgnoreTest()
+        {
+        }
+    }
+}
